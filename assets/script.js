c34b335f3a4099da804fb666fb92ecb623b9f430
aloosli <lizzwizz@tuta.io>
2023-01-26 09:53:43 +0000

    initial push with starter code

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+// When the user submits a search (by clicking the search button):
+// Store the user's input in a variable
+// Retrieve current and future weather conditions for the entered city using an API
+// Save the city name in the browser's local storage
+
+// Use the data from the API to fill in the current weather section:
+// Show the city name and today's date
+// Show the current temperature
+// Show the wind speed and direction
+// Show the humidity
+
+
+// Use the data from the API to fill in the five-day forecast:
+// Show the date for each day
+// Show an icon representing the weather conditions for each day
+// Show the temperature for each day
+// Show the wind speed and direction for each day
+// Show the humidity for each day
+
+// Using data stored in local storage, create a button in the search area for city history
+// When clicked, this button should display the current and future weather conditions for the previously searched city.
