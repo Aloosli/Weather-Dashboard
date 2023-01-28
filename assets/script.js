560389d2c0a3a6ddd5935df17acd29560c4b7c9e
aloosli <lizzwizz@tuta.io>
2023-01-28 11:36:41 +0000

    removde pseudocode, changed all var to let, used const where possible, improved some comments

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,19 @@
-// Pseudocode:
-// When the user submits a search (by clicking the search button):
-// Store the user's input in a variable
-// Retrieve current and future weather conditions for the entered city using an API
-// Save the city name in the browser's local storage
-
-// Use the data from the API to fill in the current weather section:
-// Show the city name and today's date
-// Show the current temperature
-// Show the wind speed and direction
-// Show the humidity
-
-// Use the data from the API to fill in the five-day forecast:
-// Show the date for each day
-// Show an icon representing the weather conditions for each day
-// Show the temperature for each day
-// Show the wind speed and direction for each day
-// Show the humidity for each day
-
-// Using data stored in local storage, create a button in the search area for city history
-// When clicked, this button should display the current and future weather conditions for the previously searched city.
-// end pseudocode
-
 // Global variables
-apiKey = "9ae8216bea4ff853455ec69cc8c92110";
-var cityName = "";
-var cityHistory = [];
-var currentDay = moment().format("Do MMM YYYY");
+const apiKey = "9ae8216bea4ff853455ec69cc8c92110";
+const cityHistory = [];
+const currentDay = moment().format("Do MMM YYYY");
+let cityName = "";
+
 // Function to create a button in the search area for city history
+//------------------------------------------------------------
+
 function createCityButtons(city) {
   // check if new city is already in the cityHistory array
   if (cityHistory.indexOf(city) === -1) {
     // if not, add it to the array
     cityHistory.push(city);
     // create a button for the new city
-    var cityButton = $("<button>");
+    let cityButton = $("<button>");
     // add a class to the button
     cityButton.addClass("city-button ");
     // add the city name to the button
@@ -42,9 +22,11 @@ function createCityButtons(city) {
     $(".city-buttons").append(cityButton);
   }
 }
-// get coordinates
+// Get coordinates and check if city is valid
+//------------------------------------------------------------
+
 function getCoordinates(city) {
-  var geolocationURL =
+  let geolocationURL =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
     "&limit=1&type=like&appid=" +
@@ -55,21 +37,21 @@ function getCoordinates(city) {
   }).then(function (response) {
     if (response.length !== 0) {
       cityName = response[0].name;
-      var lat = response[0].lat;
-      var lon = response[0].lon;
+      let lat = response[0].lat;
+      let lon = response[0].lon;
       getWeather(lat, lon);
-      // call function createCityButtons(city)
-      createCityButtons(city);
-      console.log(response);
     } else {
       alert("Please enter a valid city name");
+    }
+    if (response.length !== 0) {
+      createCityButtons(cityName);
     }
   });
 
   // Function to retrieve current and future weather conditions for the entered city using an API
   function getWeather(lat, lon) {
     // Constructing a queryURL using the city name
-    var queryURL =
+    let queryURL =
       "https://api.openweathermap.org/data/2.5/forecast?lat=" +
       lat +
       "&lon=" +
@@ -88,10 +70,6 @@ function getCoordinates(city) {
     })
       // After data comes back from the request
       .then(function (response) {
-        // Log the queryURL
-        console.log(queryURL);
-        // Log the resulting object
-        console.log(response);
         // Transfer content to HTML
         // clear the #today div
         $("#today").empty();
@@ -120,10 +98,12 @@ function getCoordinates(city) {
         );
 
         // Create 5 forecast cards and append them to <section id="forecast-container"
+        //------------------------------------------------------------
+
         // clear the #forecast-container div
         $("#forecast-container").empty();
-        for (var i = 0; i < 5; i++) {
-          var forecastCard = $("<div>");
+        for (let i = 0; i < 5; i++) {
+          let forecastCard = $("<div>");
           forecastCard.addClass("card mx-auto text-white m-2 text-center");
           forecastCard.html(`
                 <img class="card-img-top mx-auto" src="https://openweathermap.org/img/w/${
@@ -147,12 +127,14 @@ function getCoordinates(city) {
   }
 }
 
-// function to capitalize the first letter of city names
+// Function to capitalize the first letter of city names
+//------------------------------------------------------------
+
 function capitalizeWords(inputString) {
   // Convert the input string to lowercase and split it into an array of words
-  var words = inputString.toLowerCase().split(" ");
+  let words = inputString.toLowerCase().split(" ");
   // Loop through the array of words
-  for (var i = 0; i < words.length; i++) {
+  for (let i = 0; i < words.length; i++) {
     // Capitalize the first letter of each word
     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
   }
@@ -160,6 +142,7 @@ function capitalizeWords(inputString) {
   return words.join(" ");
 }
 // Store the user's input in a variable
+//------------------------------------------------------------
 $("#search-button").on("click", function (event) {
   event.preventDefault();
   city = capitalizeWords($("#search-input").val());
@@ -168,15 +151,14 @@ $("#search-button").on("click", function (event) {
     alert("Please enter a city name");
     return;
   }
-  console.log(city);
   // Retrieve current and future weather conditions for the entered city using an API
   getCoordinates(city);
-
   // Save the city name in the browser's local storage
   localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
 });
 
 // event listener for city buttons
+//------------------------------------------------------------
 $(document).on("click", ".city-button", function () {
   // Get the city name from the button text
   city = $(this).text();
